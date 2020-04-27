e0185c6668e0cd5989c73381cd3043fb28474d3e
Keith Jedlicka <kjedlicka@gmail.com>
2020-04-27 16:51:17 -0400

    generate HTML file

--- practice.js
+++ practice.js
@@ -1,11 +1,26 @@
-// this will print the number 5 five times
-for (var i = 0; i < 5; i += 1) {
-    setTimeout(() => console.log(i), 1000);
-  }
-  
-  console.log('=================');
-  
-  // this will print 0, 1, 2, 3, 4
-  for (let i = 0; i < 5; i += 1) {
-    setTimeout(() => console.log(i), 1000);
-  }
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+
+
+const generatePage = (name, github) => {
+  return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
+};
+
+console.log(name, github);
+
+console.log(generatePage(name, github));
